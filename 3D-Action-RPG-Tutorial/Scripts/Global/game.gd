f705a2b358903333d126e30a4b46d985eba045b4
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2024-10-22 23:10:59 +0800

    Adds the initial inventory slot to the project.

--- 3D-Action-RPG-Tutorial/Scripts/Global/game.gd
+++ 3D-Action-RPG-Tutorial/Scripts/Global/game.gd
@@ -1,2 +1,18 @@
 extends Node
 
+var items: Dictionary = {
+	"long sword": preload("res://Scenes/player/GUI/inventory/long_sword.tres"),
+	"iron sword": preload("res://Scenes/player/GUI/inventory/iron_sword.tres"),
+	"small potion": preload("res://Scenes/player/GUI/inventory/small_potion.tres")
+}
+
+var gold: int = 100
+var player_health: int  = 5
+var player_health_max: int = 5
+var right_hand_equipped: ItemData
+var body_equipped: ItemData
+
+var player_damage: int = 0
+var player_defense: int = 0
+
+
--- 3D-Action-RPG-Tutorial/Scripts/Player/GUI/inventory/inventory_main.gd
+++ 3D-Action-RPG-Tutorial/Scripts/Player/GUI/inventory/inventory_main.gd
@@ -1,6 +1,11 @@
 extends Node
 
+@export var inventory_size = 24
+@onready var grid = get_node("grid")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var _slot = InventorySlot.new()
-	add_child(_slot)
+	for i in range(inventory_size):
+		var _slot = InventorySlot.new()
+		_slot.init(ItemData.Type.MAIN, Vector2(32, 32))
+		grid.add_child(_slot)
