35de40352f61bd37dd85a354d989f01d48a95753
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2024-10-27 22:00:24 +0800

    Adds the ability to add item to the inventory.

--- 3D-Action-RPG-Tutorial/Scripts/Player/GUI/inventory/inventory_main.gd
+++ 3D-Action-RPG-Tutorial/Scripts/Player/GUI/inventory/inventory_main.gd
@@ -9,3 +9,27 @@ func _ready():
 		var _slot = InventorySlot.new()
 		_slot.init(ItemData.Type.MAIN, Vector2(32, 32))
 		grid.add_child(_slot)
+	add_item("long sword")
+	add_item("small potion")
+
+func add_item(item_name: String) -> void:
+	var item = InventoryItem.new()
+	item.init(Game.items[item_name])
+	if item.data.stackable:
+		for i in inventory_size:
+			if grid.get_child(i).get_child_count() > 0:
+				if grid.get_child(i).get_child(0).data == item.data:
+					# update the counter
+					grid.get_child(i).get_child(0).data.count += 1
+					# if the label
+					grid.get_child(i).get_child(0).get_child(0).set_text(str(grid.get_child(i).get_child(0).data.count))
+					break
+			else:
+				grid.get_child(i).add_child(item)
+				break
+	else:
+		for i in inventory_size:
+			if grid.get_child(i).get_child_count() == 0:
+				grid.get_child(i).add_child(item)
+				break
+	
